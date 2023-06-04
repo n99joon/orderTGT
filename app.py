455f7a710f2f8c2bbf7b7213c822a74fd74f1e93
CaiserD <ctl78787@gmail.com>
2023-06-05 02:41:07 +0900

    Changed for testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 
 app = Flask(__name__)
 
@@ -8,23 +8,22 @@ def hello():
     #return 'Hello World!'
 
 # # Temporary data store for user records
-# users = []
+users = []
 
-# @app.route('/api/users', methods=['POST'])
-# def create_user():
-#     data = request.get_json()
+@app.route('/api/users', methods=['GET', 'POST'])
+def create_user():
+    data = request.get_json()
+    if 'name' not in data or 'email' not in data:
+        return jsonify(message='Name and email are required.'), 400
 
-#     if 'name' not in data or 'email' not in data:
-#         return jsonify(message='Name and email are required.'), 400
-
-#     name = data['name']
-#     email = data['email']
+    name = data['name']
+    email = data['email']
 
 #     # Create a new user record
-#     user = {'name': name, 'email': email}
-#     users.append(user)
+    user = {'name': name, 'email': email}
+    users.append(user)
 
-#     return jsonify(message='User created successfully.', user=user), 201
+    return jsonify(message='User created successfully.', user=user), 201
 
 #this function should return the names of all users in the array users.
 #Finish this code.
