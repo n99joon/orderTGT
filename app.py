9ff2aa84e2b2900e521f68e53475e29ad7d78ddf
CaiserD <ctl78787@gmail.com>
2023-06-05 21:09:28 +0900

    Fixed all codes

--- app.py
+++ app.py
@@ -2,45 +2,35 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
-
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message='Hello, world!')
-    #return 'Hello World!'
 
-# # Temporary data store for user records
+# Temporary data store for user records
 users = []
 
 @app.route('/api/users', methods=['GET', 'POST'])
-#415 error -> Need to add header for application/json
 def create_user():
-    header = {
-        'Content_type': 'application/json'
-    }
-    response = response.request('http://localhost:5000/api/users',
-                            headers = header,
-                            verify = False)
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'name' not in data or 'email' not in data:
+            return jsonify(message='Name and email are required.'), 400
+
+        name = data['name']
+        email = data['email']
+
+        # Create a new user record
+        user = {'name': name, 'email': email}
+        users.append(user)
+
+        return jsonify(message='User created successfully.', user=user), 201
     
-    
-    data = request.get_json()
-    if 'name' not in data or 'email' not in data:
-        return response.jsonify(message='Name and email are required.'), 400
+    elif request.method == 'GET':
+        return jsonify(users=users)
 
-    name = data['name']
-    email = data['email']
-
-#     # Create a new user record
-    user = {'name': name, 'email': email}
-    users.append(user)
-
-    return response.jsonify(message='User created successfully.', user=user), 201
-
-#this function should return the names of all users in the array users.
-#Finish this code.
-# @app.route('/api/getUsers', methods=['GET'])
-# def user():
-#     return
+@app.route('/api/getUsers', methods=['GET'])
+def get_users():
+    return jsonify(users=users)
 
 if __name__ == '__main__':
     app.run()
