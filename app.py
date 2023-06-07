6000f6ae2afc36c85842c13a0f8616467b73c4fe
CaiserD <ctl78787@gmail.com>
2023-06-08 05:06:55 +0900

    Successful

--- app.py
+++ app.py
@@ -5,33 +5,32 @@ app = Flask(__name__)
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message='Hello, world!')
+    #return 'Hello World!'
 
 # Temporary data store for user records
 users = []
 
-@app.route('/api/users', methods=['GET', 'POST'])
-
+@app.route('/api/users', methods=['POST'])
 def create_user():
-    if request.method == 'POST':
-        data = request.get_json()
-        if 'name' not in data or 'email' not in data:
-            return jsonify(message='Name and email are required.'), 400
+    data = request.get_json()
 
-        name = data['name']
-        email = data['email']
+    if 'name' not in data or 'email' not in data:
+        return jsonify(message='Name and email are required.'), 400
 
-        # Create a new user record
-        user = {'name': name, 'email': email}
-        users.append(user)
+    name = data['name']
+    email = data['email']
 
-        return jsonify(message='User created successfully.', user=user), 201
-    elif request.method == 'GET':
-        return jsonify(message='GET')
-    
+    # Create a new user record
+    user = {'name': name, 'email': email}
+    users.append(user)
 
+    return jsonify(message='User created successfully.', user=user), 201
+
+#this function should return the names of all users in the array users.
+#Finish this code.
 @app.route('/api/getUsers', methods=['GET'])
-def get_users():
-    return jsonify(users=users)
+def user():
+    return
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
