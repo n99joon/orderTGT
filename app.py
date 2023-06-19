7a677c36797d3b634c5fec9c8716d2c04f951ff9
CaiserD <ctl78787@gmail.com>
2023-06-19 22:58:37 +0900

    Added Database into the Code

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask, jsonify, request
+import mysql.connector
 
 app = Flask(__name__)
+
+# Establish a connection to the MySQL server
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='09161016',
+    database='mydatabase'
+)
+
+# Create a cursor to interact with the database
+cursor = db.cursor()
+
+# Create the users table if it doesn't exist
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255)
+)
+'''
+cursor.execute(create_table_query)
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message='Hello, world!')
-
-# Temporary data store for user records
-users = []
 
 @app.route('/api/users', methods=['POST'])
 def create_user():
@@ -19,18 +38,33 @@ def create_user():
     name = data['name']
     email = data['email']
 
+    # Insert the new user into the database
+    insert_query = 'INSERT INTO users (name, email) VALUES (%s, %s)'
+    values = (name, email)
+    cursor.execute(insert_query, values)
+    db.commit()
+
+    # Get the inserted user's ID
+    user_id = cursor.lastrowid
+
     # Create a new user record
-    user = {'name': name, 'email': email}
-    users.append(user)
+    user = {'id': user_id, 'name': name, 'email': email}
 
     return jsonify(message='User created successfully.', user=user), 201
 
 @app.route('/api/getUsers', methods=['GET'])
 def get_users():
-    if len(users) == 0:
+    # Retrieve all users from the database
+    select_query = 'SELECT * FROM users'
+    cursor.execute(select_query)
+    result = cursor.fetchall()
+
+    if len(result) == 0:
         return jsonify(message='No users found.')
-    
-    user_names = [user['name'] for user in users]
+
+    # Extract user names from the result
+    user_names = [user[1] for user in result]
+
     return jsonify(users=user_names)
 
 if __name__ == '__main__':
